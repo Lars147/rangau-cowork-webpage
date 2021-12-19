b3d478bc1cb79da9643143b2d00a00c5043083b7
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-12-19 18:47:04 +0100

    added Navbar

--- src/pages/umfrage.tsx
+++ src/pages/umfrage.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Head from 'next/head';
 
+import Navbar from '../components/Navbar';
+
 const Survey = () => {
   return (
     <>
       <Head>
         <title>Rangau Cowork - Umfrage</title>
       </Head>
+      <Navbar />
       <div className="content-center bg-green-50">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSeOwEoOFLSm8ROR5Np8E2EsB9RHY7bSZU5cRtRQ0D71l5GuCA/viewform?embedded=true"
