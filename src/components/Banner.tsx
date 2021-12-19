162aaef5025ac37cca880c40b58a19f0d110b93a
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-12-19 18:42:15 +0100

    added passHref to Banner Link

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,7 +8,7 @@ const SimpleBanner = () => {
   return (
     <div className="bg-green-400 text-center">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8 font-medium text-white">
-        <Link href="/umfrage">
+        <Link href="/umfrage" passHref>
           <a className="after:absolute after:inset-0">{banner.text}</a>
         </Link>
       </div>
