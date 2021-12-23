00e90e0fb1b763886dff06aeb6f1bd31c8dc2a4b
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-12-23 19:44:45 +0100

    added people to teams with default picture

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -15,7 +15,7 @@ export default function TeamSection() {
           </div>
           <ul
             role="list"
-            className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3 lg:gap-8"
+            className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols lg:gap-8"
           >
             <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
               <div className="space-y-6 xl:space-y-10">
@@ -48,27 +48,127 @@ export default function TeamSection() {
                             strokeLinecap="round"
                             strokeLinejoin="round"
                             strokeWidth={2}
-                            d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                           />
                         </svg>
                       </a>
                     </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
+              <div className="space-y-6 xl:space-y-10">
+                <img
+                  className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                  src="/assets/images/rangau_coworking_logo.png"
+                  alt=""
+                />
+                <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
+                  <div className="font-medium text-lg leading-6 space-y-1">
+                    <h3 className="text-white">Lukas Engel</h3>
+                    <p className="text-emerald-400">Rimbach</p>
+                  </div>
+
+                  <ul role="list" className="flex justify-center space-x-5">
                     <li>
                       <a
-                        href="https://www.linkedin.com/in/jonasmiederer/"
+                        href="mailto:lukas.engel@rangau-cowork.de"
                         className="text-gray-400 hover:text-gray-300"
                       >
-                        <span className="sr-only">LinkedIn</span>
+                        <span className="sr-only">E-Mail</span>
                         <svg
-                          className="w-5 h-5"
-                          aria-hidden="true"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
                         >
                           <path
-                            fillRule="evenodd"
-                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                            clipRule="evenodd"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
+              <div className="space-y-6 xl:space-y-10">
+                <img
+                  className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                  src="/assets/images/rangau_coworking_logo.png"
+                  alt=""
+                />
+                <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
+                  <div className="font-medium text-lg leading-6 space-y-1">
+                    <h3 className="text-white">Tim Oberländer</h3>
+                    <p className="text-emerald-400">Neuhof</p>
+                  </div>
+
+                  <ul role="list" className="flex justify-center space-x-5">
+                    <li>
+                      <a
+                        href="mailto:tim.oberlaender@rangau-cowork.de"
+                        className="text-gray-400 hover:text-gray-300"
+                      >
+                        <span className="sr-only">E-Mail</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left">
+              <div className="space-y-6 xl:space-y-10">
+                <img
+                  className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                  src="/assets/images/rangau_coworking_logo.png"
+                  alt=""
+                />
+                <div className="space-y-2 xl:flex xl:items-center xl:justify-between">
+                  <div className="font-medium text-lg leading-6 space-y-1">
+                    <h3 className="text-white">Lars Heinen</h3>
+                    <p className="text-emerald-400">Markt Erlbach</p>
+                  </div>
+
+                  <ul role="list" className="flex justify-center space-x-5">
+                    <li>
+                      <a
+                        href="mailto:lars.heinen@rangau-cowork.de"
+                        className="text-gray-400 hover:text-gray-300"
+                      >
+                        <span className="sr-only">E-Mail</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                           />
                         </svg>
                       </a>
