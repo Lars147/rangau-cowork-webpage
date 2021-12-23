cf3628aec314acececf7d0afe46438b26e52e3cd
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-12-23 19:44:27 +0100

    updated texts with highlighting

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,7 +6,7 @@ const Features = () => {
   const [showAllDescription, setShowAllDescription] = useState(false);
 
   const { features } = config;
-  const { title, subtitle, items: featuresList } = features;
+  const { title, subtitle } = features;
   return (
     <div className={`py-12 bg-background min-h-1/2`} id="features">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -21,8 +21,11 @@ const Features = () => {
           </p>
           <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
             In Städten ist Coworking längst etabliert. Coworking auf dem Land
-            bietet viel unerschöpftes Potential. Gerade im Rangau fehlt ein
-            solches zukunftsweisendes Angebot bisher.
+            bietet viel unerschöpftes Potential. Gerade im{' '}
+            <span className="font-bold underline decoration-2 decoration-emerald-400">
+              Rangau fehlt ein solches zukunftsweisendes Angebot
+            </span>{' '}
+            bisher.
             <div
               className={
                 showAllDescription
@@ -36,10 +39,32 @@ const Features = () => {
             <div className={showAllDescription ? '' : 'hidden'}>
               Die Bedürfnisse der beteiligten Akteure sind jedoch vielfältig und
               bringen deshalb ein breites Spektrum an Herausforderungen mit
-              sich. Die Interessen von Nutzern, Gründern und Betreibern haben
-              für uns die gleiche Relevanz wie die Belange von Politik und
-              Zivilgesellschaft. Auch Investoren und Unternehmen auf der Suche
-              nach Arbeitsorten für ihre Angestellten binden wir aktiv ein.
+              sich. Die Interessen von{' '}
+              <span className="font-bold underline decoration-2 decoration-emerald-400">
+                Nutzern
+              </span>
+              ,{' '}
+              <span className="font-bold underline decoration-2 decoration-emerald-400">
+                Gründern
+              </span>{' '}
+              und{' '}
+              <span className="font-bold underline decoration-2 decoration-emerald-400">
+                Betreibern
+              </span>{' '}
+              haben für uns die gleiche Relevanz wie die Belange von{' '}
+              <span className="font-bold underline decoration-2 decoration-emerald-400">
+                Politik
+              </span>{' '}
+              und{' '}
+              <span className="font-bold underline decoration-2 decoration-emerald-400">
+                Zivilgesellschaft
+              </span>
+              . Auch{' '}
+              <span className="font-bold underline decoration-2 decoration-emerald-400">
+                Investoren
+              </span>{' '}
+              und Unternehmen auf der Suche nach Arbeitsorten für ihre
+              Angestellten binden wir aktiv ein.
               <br />
               Wir kennen die Wünsche aller Beteiligten und bieten Lösungen für
               deren spezifische Anforderungen zum Coworking im Rangau.
@@ -59,28 +84,115 @@ const Features = () => {
 
         <div className="mt-10">
           <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            {featuresList.map((feature) => (
-              <div key={feature.name} className="relative">
-                <dt>
-                  <div
-                    className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-emerald-400 border-4`}
-                  >
-                    <img
-                      className={`inline-block h-6 w-6 rounded-full`}
-                      src={feature.icon}
-                      alt={feature.name}
-                    />
-                    {feature?.svg}
-                  </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                    {feature.name}
-                  </p>
-                </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">
-                  {feature.description}
-                </dd>
-              </div>
-            ))}
+            <div className="relative">
+              <dt>
+                <div
+                  className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-emerald-400 border-4`}
+                >
+                  <img
+                    className={`inline-block h-6 w-6 rounded-full`}
+                    src="/assets/images/location-marker.svg"
+                    alt="location"
+                  />
+                  {''}
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Lokal
+                </p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">
+                Wir wissen, wie Coworking auch außerhalb von Städten
+                funktioniert. Eine starke{' '}
+                <span className="font-bold underline decoration-2 decoration-emerald-400">
+                  Bindung an die lokale Wirtschaft
+                </span>{' '}
+                ist ein Kernfaktor zum Erfolg. Der Rangau bietet mit seiner
+                vielfältigen Wirtschaftskraft enormes Potential in allen
+                Aspekten regionaler Wertschöpfung.
+              </dd>
+            </div>
+            <div className="relative">
+              <dt>
+                <div
+                  className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-emerald-400 border-4`}
+                >
+                  <img
+                    className={`inline-block h-6 w-6 rounded-full`}
+                    src="/assets/images/chip.svg"
+                    alt="chip"
+                  />
+                  {''}
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Digital
+                </p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">
+                Digitale Konzepte weisen den Weg in die Zukunft und ermöglichen
+                räumlich flexibilisiertes Arbeiten. Mit unserer Expertise
+                ermöglichen wir die effiziente,{' '}
+                <span className="font-bold underline decoration-2 decoration-emerald-400">
+                  digitale Lösung von Problemstellungen
+                </span>{' '}
+                in Coworking-Spaces.
+              </dd>
+            </div>
+            <div className="relative">
+              <dt>
+                <div
+                  className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-emerald-400 border-4`}
+                >
+                  <img
+                    className={`inline-block h-6 w-6 rounded-full`}
+                    src="/assets/images/chat-alt-2.svg"
+                    alt="chat"
+                  />
+                  {''}
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Sozial
+                </p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">
+                Wir sind davon überzeugt, dass Coworking-Projekte auf dem Land
+                nur in Gemeinschaft funktionieren. Deshalb setzen wir uns dafür
+                ein, dass alle Interessensgruppen in die Entwicklung einbezogen
+                werden.{' '}
+                <span className="font-bold underline decoration-2 decoration-emerald-400">
+                  Starke Dorfgemeinschaften
+                </span>{' '}
+                und ein{' '}
+                <span className="font-bold underline decoration-2 decoration-emerald-400">
+                  aktives Vereinsleben
+                </span>{' '}
+                bieten beste Voraussetzungen für Coworking-Projekte im Rangau.
+              </dd>
+            </div>
+            <div className="relative">
+              <dt>
+                <div
+                  className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-emerald-400 border-4`}
+                >
+                  <img
+                    className={`inline-block h-6 w-6 rounded-full`}
+                    src="/assets/images/globe.svg"
+                    alt="globe"
+                  />
+                  {''}
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Nachhaltig
+                </p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">
+                Für uns ist Nachhaltigkeit nicht nur ein Trend. Mit Beratung und
+                Betreuung über die Gründungsphase hinaus schaffen wir{' '}
+                <span className="font-bold underline decoration-2 decoration-emerald-400">
+                  langfristigen Mehrwert
+                </span>{' '}
+                für alle Coworking-Akteure.
+              </dd>
+            </div>
           </dl>
         </div>
       </div>
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -23,53 +23,108 @@ const Product = () => {
         </h1>
         <Divider />
         <div className={`md:mt-20 md:space-y-10`}>
-          {product.items.map((item, index) => {
-            if (index % 2) {
-              return (
-                <div
-                  className={`flex flex-wrap flex-col-reverse sm:flex-row`}
-                  key={index}
+          <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
+            <div className={`w-full sm:w-1/2 p-6`}>
+              <img
+                className="h-6/6 rounded-md shadow-xl"
+                src="/assets/images/work.jpg"
+                alt="work"
+              />
+            </div>
+            <div className={`w-full sm:w-1/2 p-6 mt-20`}>
+              <div className={`align-middle`}>
+                <h3
+                  className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
                 >
-                  <div className={`w-full sm:w-1/2 p-6`}>
-                    <img
-                      className="h-6/6 rounded-md shadow-xl"
-                      src={item.img}
-                      alt={item.title}
-                    />
-                  </div>
-                  <div className={`w-full sm:w-1/2 p-6 mt-20`}>
-                    <div className={`align-middle`}>
-                      <h3
-                        className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-                      >
-                        {item.title}
-                      </h3>
-                      <p className={`text-gray-600 mb-8`}>{item.description}</p>
-                    </div>
-                  </div>
-                </div>
-              );
-            }
-            return (
-              <div className={`flex flex-wrap`} key={index}>
-                <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
-                  <h3
-                    className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-                  >
-                    {item.title}
-                  </h3>
-                  <p className={`text-gray-600`}>{item.description}</p>
-                </div>
-                <div className={`w-full sm:w-1/2 p-6`}>
-                  <img
-                    className="h-6/6 rounded-md shadow-xl"
-                    src={item.img}
-                    alt={item.title}
-                  />
-                </div>
+                  Arbeitsplatz
+                </h3>
+                <p className={`text-gray-600 mb-8`}>
+                  Coworking ist für uns mehr als nur ein mobiler Arbeitsplatz.
+                  Wir verknüpfen Produktivität mit{' '}
+                  <span className="font-bold underline decoration-2 decoration-emerald-400">
+                    Wohlfühlfaktor
+                  </span>{' '}
+                  und Büroatmosphäre mit{' '}
+                  <span className="font-bold underline decoration-2 decoration-emerald-400">
+                    Community
+                  </span>
+                  , um inspirierende Orte für gemeinsames Arbeiten zu
+                  erschaffen.
+                </p>
               </div>
-            );
-          })}
+            </div>
+          </div>
+          <div className={`flex flex-wrap`}>
+            <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
+              <h3
+                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+              >
+                Gemeinschaft
+              </h3>
+              <p className={`text-gray-600`}>
+                Beziehungen sind Katalysatoren für erfolgreiches Arbeiten. Wir
+                sind davon überzeugt, dass ein{' '}
+                <span className="font-bold underline decoration-2 decoration-emerald-400">
+                  vielfältiges soziales Netzwerk
+                </span>{' '}
+                die Kreativität beflügelt und nachhaltigen Mehrwert schafft.
+                Coworking gibt Menschen die Möglichkeit, sich lokal miteinander
+                zu vernetzen.{' '}
+                <span className="font-bold underline decoration-2 decoration-emerald-400">
+                  Über Unternehmensgrenzen hinweg
+                </span>{' '}
+                entstehen so kreative Problemlösungen.
+              </p>
+            </div>
+            <div className={`w-full sm:w-1/2 p-6`}>
+              <img
+                className="h-6/6 rounded-md shadow-xl"
+                src="/assets/images/social.jpg"
+                alt="social"
+              />
+            </div>
+          </div>
+          <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
+            <div className={`w-full sm:w-1/2 p-6`}>
+              <img
+                className="h-6/6 rounded-md shadow-xl"
+                src="/assets/images/equipment-small.jpg"
+                alt="equipment"
+              />
+            </div>
+            <div className={`w-full sm:w-1/2 p-6 mt-20`}>
+              <div className={`align-middle`}>
+                <h3
+                  className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+                >
+                  Ausstattung
+                </h3>
+                <p className={`text-gray-600 mb-8`}>
+                  Ein Arbeitsplatz muss heute allen Anforderungen der modernen
+                  Arbeitswelt gerecht werden. Die Coworking-Infrastruktur
+                  ermöglicht sowohl{' '}
+                  <span className="font-bold underline decoration-2 decoration-emerald-400">
+                    fokussiertes Arbeiten
+                  </span>{' '}
+                  und konstruktive Meetings als auch zwischenmenschliche
+                  Begegnungen und gemeinschaftliches Miteinander. Nutzer von
+                  Coworking-Flächen profitieren von fortschrittlicher{' '}
+                  <span className="font-bold underline decoration-2 decoration-emerald-400">
+                    IT-Infrastruktur
+                  </span>
+                  , ergonomischem{' '}
+                  <span className="font-bold underline decoration-2 decoration-emerald-400">
+                    Arbeitsmobiliar
+                  </span>{' '}
+                  und innovativen{' '}
+                  <span className="font-bold underline decoration-2 decoration-emerald-400">
+                    Gemeinschaftsbereichen
+                  </span>
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
