e91145d2d9b14b532b50d83c4a20c7190b53a172
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-12-23 18:45:39 +0100

    make the mobile header work

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -100,13 +100,18 @@ const Menu = () => {
               </div>
               <div className="px-2 pt-2 pb-3 space-y-1">
                 {navigation.map((item) => (
-                  <a
+                  <Link
+                    spy={true}
+                    active="active"
+                    smooth={true}
+                    duration={1000}
                     key={item.name}
+                    to={item.href}
                     href={item.href}
                     className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               {/* <a
