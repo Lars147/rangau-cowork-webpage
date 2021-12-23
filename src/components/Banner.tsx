528d5df8df5ec9893c9b96633ece3808fd808bf3
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-12-23 18:45:56 +0100

    change survey link

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,8 +8,13 @@ const SimpleBanner = () => {
   return (
     <div className="bg-emerald-400 text-center">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8 font-medium text-white">
-        <Link href="/umfrage" passHref>
-          <a className="after:absolute after:inset-0">{banner.text}</a>
+        <Link
+          href="https://docs.google.com/forms/d/e/1FAIpQLSeOwEoOFLSm8ROR5Np8E2EsB9RHY7bSZU5cRtRQ0D71l5GuCA/viewform?usp=sf_link"
+          passHref
+        >
+          <a className="after:absolute after:inset-0" target="_blank">
+            {banner.text}
+          </a>
         </Link>
       </div>
     </div>
