52785aa85e11e7704cc748684fd3c52fed4ddc3d
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2021-12-24 12:55:40 +0100

    added speaker icons on banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,4 @@
+import { SpeakerphoneIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 
 import config from '../config/index.json';
@@ -12,8 +13,19 @@ const SimpleBanner = () => {
           href="https://docs.google.com/forms/d/e/1FAIpQLSeOwEoOFLSm8ROR5Np8E2EsB9RHY7bSZU5cRtRQ0D71l5GuCA/viewform?usp=sf_link"
           passHref
         >
-          <a className="after:absolute after:inset-0" target="_blank">
-            {banner.text}
+          <a
+            className="after:absolute after:inset-0 flex justify-center"
+            target="_blank"
+          >
+            <SpeakerphoneIcon
+              className="h-6 w-6 text-white"
+              aria-hidden="true"
+            />
+            <span className="mx-4">{banner.text}</span>
+            <SpeakerphoneIcon
+              className="h-6 w-6 text-white scale-x-[-1]"
+              aria-hidden="true"
+            />
           </a>
         </Link>
       </div>
